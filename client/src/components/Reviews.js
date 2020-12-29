8eeec6ceb3167be1ade38fbf84937164b4332a49
horsemaker <yash_ghodekar@yahoo.in>
2020-12-29 18:31:42 +0530

    Deployment.

---
+++ client/src/components/Reviews.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import StarRating from './StarRating';
+
+const Reviews = ({ reviews }) => {
+    return (
+        <div className="row row-cols-3 mb-2">
+            {reviews.map((review) => {
+                return (
+                    <div  className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+                        <div className="card-header d-flex justify-content-between">
+                            <span>
+                                {review.name}
+                            </span>
+                            <span>
+                                <StarRating rating={review.rating} />
+                            </span>
+                        </div>
+                        <div className="card-body">
+                            <p className="card-text">
+                                {review.review}
+                            </p>
+                        </div>
+                    </div>
+                );
+            })}
+            {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+                <div className="card-header d-flex justify-content-between">
+                    <span>
+                        Samargeet
+                    </span>
+                    <span>
+                        <StarRating rating={3} />
+                    </span>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Yes! Yes! Yes!
+                    </p>
+                </div>
+            </div> */}
+
+            {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+                <div className="card-header d-flex justify-content-between">
+                    <span>
+                        Samargeet
+                    </span>
+                    <span>
+                        <StarRating rating={3} />
+                    </span>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Yes! Yes! Yes!
+                    </p>
+                </div>
+            </div> */}
+
+            {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+                <div className="card-header d-flex justify-content-between">
+                    <span>
+                        Samargeet
+                    </span>
+                    <span>
+                        <StarRating rating={3} />
+                    </span>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Yes! Yes! Yes!
+                    </p>
+                </div>
+            </div> */}
+
+            {/* <div className="card text-white bg-primary mb-3 mr-4" style={{maxWidth: "30%"}}>
+                <div className="card-header d-flex justify-content-between">
+                    <span>
+                        Samargeet
+                    </span>
+                    <span>
+                        <StarRating rating={3} />
+                    </span>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">
+                        Yes! Yes! Yes!
+                    </p>
+                </div>
+            </div> */}
+        </div>
+    )
+}
+
+export default Reviews;
