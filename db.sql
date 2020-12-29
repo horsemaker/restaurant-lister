5717c65d92f8faad7bc6ddee786b96c1a0e178e5
horsemaker <yash_ghodekar@yahoo.in>
2020-12-29 18:07:25 +0530

    Comments in db.sql

--- db.sql
+++ db.sql
@@ -9,6 +9,7 @@ CREATE TABLE restaurants (
 -- commands to help => \d , \l
 
 -- Creating 'reviews' table in yelp
+
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
@@ -17,13 +18,13 @@ CREATE TABLE reviews (
     rating INT NOT NULL check( rating >=1 and rating <=5 )
 );
 
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Samargeet', 'Yes', 4);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Aditya Mahak', 'Mai to so raha tha', 5);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Jack', 'Mai to ro raha tha', 1);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (8, 'Pappya', 'Bara Ka', 4);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (8, 'Peti', 'Priority Null', 1);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (10, 'Pappu', 'Paisa Hi Paisa Hoga', 5);
-INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (13, 'Jexus', 'Lamb Lamb Lamb Lamb', 5);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Samargeet', 'Yes', 4);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Aditya Mahak', 'Mai to so raha tha', 5);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (9, 'Jack', 'Mai to ro raha tha', 1);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (8, 'Pappya', 'Bara Ka', 4);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (8, 'Peti', 'Priority Null', 1);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (10, 'Pappu', 'Paisa Hi Paisa Hoga', 5);
+-- INSERT INTO reviews (restaurant_id, name, review, rating) VALUES (13, 'Jexus', 'Lamb Lamb Lamb Lamb', 5);
 
 
 --  9 | Samargeet    | Yes                      |      4
