e222a927a098cd24e8996a65cb78fc1714be04a2
horsemaker <yash_ghodekar@yahoo.in>
2020-12-29 20:12:49 +0530

    Deployment.

--- db/index.js
+++ db/index.js
@@ -1,24 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const devConfig = {
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    host: process.env.PGHOST,
-    database: process.env.PGDATABASE,
-    port: process.env.PGPORT,
-};
+// const devConfig = {
+//     user: process.env.PGUSER,
+//     password: process.env.PGPASSWORD,
+//     host: process.env.PGHOST,
+//     database: process.env.PGDATABASE,
+//     port: process.env.PGPORT,
+// };
 
-// const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+const devConfig = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
 
-const proConfig = {
-    connectionString: process.env.DATABASE_URL
-}; // heroku addons
+const proConfig = process.env.DATABASE_URL; // heroku addons
 
 // const pool = new Pool({
 //     connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig
 // });
-const pool = new Pool(process.env.NODE_ENV === "production" ? proConfig : devConfig);
+const pool = new Pool({connectionString: process.env.NODE_ENV === "production" ? proConfig : devConfig});
 
 module.exports = {
     query: (text, params) => pool.query(text, params),
